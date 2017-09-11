217131c0b9d2461c31aaba0701652714176991b9
Xiayc <xiayc@shanghaitech.edu.cn>
2017-09-11 17:53:37 +0800

    clear

--- MathMLToCSharp/Entities/Mtable.cs
+++ MathMLToCSharp/Entities/Mtable.cs
@@ -1,36 +1,23 @@
 using System.Text;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace MathMLToCSharp.Entities
 {
-    /// <summary>
-    /// A table. Typically used for matrices.
-    /// </summary>
-    public class Mtable : WithBuildableContents
+  /// <summary>
+  /// A table. Typically used for matrices.
+  /// </summary>
+  public class Mtable : WithBuildableContents
+  {
+    public Mtable(IBuildable[] contents) : base(contents) {}
+    public override void Visit(StringBuilder sb, BuildContext context)
     {
-        static int level = 0;
-        public Mtable(IBuildable[] contents) : base(contents) { }
-        public override void Visit(StringBuilder sb, BuildContext context)
-        {
-            level++;
-            Matrix<double> m = DenseMatrix.OfArray(new double[,] { { 1, 2 }, { 3, 4 } });
-            var c = m.Determinant();
-            if (level == 1)
-                sb.Append("DenseMatrix.OfArray(new double[,] {");//matrix
-            else
-                sb.Append("{");//matrix
-            for (int i = 0; i < contents.Length; ++i)
-            {
-                contents[i].Visit(sb, context);
-                if (i + 1 != contents.Length)
-                    sb.Append(", ");
-            }
-            if (level == 1)
-                sb.Append("})");
-            else
-                sb.Append("}");
-            level--;
-        }
+      sb.Append("{");//matrix
+      for (int i = 0; i < contents.Length; ++i)
+      {
+        contents[i].Visit(sb, context);
+        if (i + 1 != contents.Length)
+          sb.Append(", ");
+      }
+      sb.Append("}");
     }
+  }
 }
